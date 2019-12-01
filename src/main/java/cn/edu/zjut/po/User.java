368b1c0acbc318f5f81496a7a39f613dc090e76c
tony-coder <1606771552@qq.com>
2019-12-01 16:40:03 +0800

    添加用户模型User（基础）、添加UserService接口

--- src/main/java/cn/edu/zjut/po/User.java
+++ src/main/java/cn/edu/zjut/po/User.java
@@ -1,4 +1,102 @@
 package cn.edu.zjut.po;
 
-public class User{
+import java.util.Date;
+
+/**
+ * 用户信息实体类
+ */
+public class User {
+    private Integer id;         //用户id
+    private String username;    //用户名
+    private String password;    //密码
+    private String email;       //邮箱
+    private String phoneNum;    //手机号
+    private Date createTime;    //注册日期
+    private Date updateTime;    //更新日期
+    private Integer credit;     //积分
+    private String avatar;      //头像
+    private Byte type;          //用户类型
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getCredit() {
+        return credit;
+    }
+
+    public void setCredit(Integer credit) {
+        this.credit = credit;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Byte getType() {
+        return type;
+    }
+
+    public void setType(Byte type) {
+        this.type = type;
+    }
 }
---
+++ src/main/java/cn/edu/zjut/service/UserService.java
@@ -0,0 +1,58 @@
+package cn.edu.zjut.service;
+
+import cn.edu.zjut.po.User;
+
+public interface UserService {
+    /**
+     * 用户注册
+     */
+    public boolean addUser(User user);
+
+    /**
+     * 登录验证
+     */
+    public int login(String username, String password);
+
+    /**
+     * 添加积分
+     */
+    public boolean addCredit(Integer points, Integer id);
+
+    /**
+     * 检查username是否存在
+     *
+     * @param username
+     * @return
+     */
+    public boolean existUsername(String username);
+
+    /**
+     * 获取用户信息 by userName
+     *
+     * @param username
+     * @return
+     */
+    public User getUserByUsername(String username);
+
+
+    /**
+     * 获取用户信息 by id
+     *
+     * @param id
+     * @return
+     */
+    public User getUserById(Integer id);
+
+    /**
+     * 获取用户数
+     */
+    public int getUserCount();
+
+    /**
+     * 更新用户
+     *
+     * @param user
+     * @return
+     */
+    public boolean updateUser(User user);
+}
